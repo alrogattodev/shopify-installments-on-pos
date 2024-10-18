c327ae984bc7449eccce54e6a02816ac4a79e2ea
Al Rogatto <alrogattodev@gmail.com>
2024-10-18 11:03:21 -0300

    Adding function to add installments information on order note (engraving)

--- installments-on-checkout/extensions/installments-pos/src/Modal.jsx
+++ installments-on-checkout/extensions/installments-pos/src/Modal.jsx
@@ -1,56 +1,53 @@
 import React, {useState, useEffect, ReactNode} from 'react';
-import { reactExtension, Text, Navigator, RadioButtonList, Screen, ScrollView, Button, Stack, useApi } from '@shopify/ui-extensions-react/point-of-sale';
-
+import { reactExtension, Text, Navigator, RadioButtonList, Screen, ScrollView, Button, useApi } from '@shopify/ui-extensions-react/point-of-sale';
+console.error("WELCOME");
 const SmartGridModal = () => {
   const api = useApi();
-  const [selected, setSelected] =
-    React.useState('');
 
-  const subtotal = api.cart.subscribable.initial.subtotal;
+  const [selected, setSelected] = useState('');
+  const [subtotal, setSubtotal] = useState(0);
+  //subtotal = api.cart.subscribable.initial.subtotal;
   useEffect(() => {
-    const fetchCartSubtotal = async () => {
-      const cart = await api.cart.fetch();
-      setSubtotal(cart.subtotal);
-    };
-
-    fetchCartSubtotal();
+    // check if subtotal is available
+    if (api.cart && api.cart.subscribable && api.cart.subscribable.initial) {
+      setSubtotal(api.cart.subscribable.initial.subtotal);
+    } else {
+      console.log('Subtotal não disponível ainda');
+    }
   }, [api]);
-
-  const textElement = (
-    count,
-  ) => {
-    return (
-      <Stack
-        direction="vertical"
-        paddingHorizontal="Small"
-        paddingVertical="Small"
-      >
-        <Text>{`Opção ${count} `}</Text>
-      </Stack>
-    )
-  }
-  const onButtonPress = (type, title, amount) => {
-    // You can apply a discount through the cart API
-    api.cart.applyCartDiscount(type, title, amount);
-
-    // You can show a toast to notify the user of something
-    api.toast.show('Discount applied');
-  }
-
   const parcelasOptions = Array.from({ length: 18 }, (_, count) => 
     `${count + 1}x de R$${(subtotal / (count + 1)).toFixed(2)}`
   );
 
+  const aplicarParcelas = async () => {
+    if (!selected) {
+      await api.cart.updateNote('Teste de nota fixa');      
+      api.toast.show('Por favor, selecione o número de parcelas.');
+      return;
+    }
+
+    // add description
+    try { 
+      await api.cart.addCartProperties({Engraving: `Forma de Pagamento: ${selected}`})
+    } catch (error) {
+      console.error('Erro ao aplicar parcelas:', error);
+    }
+  };
+ 
+  
   return (
     <Navigator>
-      <Screen name='Discounts' title='Available Discounts'>
-        <RadioButtonList
-          items={parcelasOptions}
-          onItemSelected={setSelected}
-          initialSelectedItem={selected}
-        />
-        <Text>{`Você selecionou: ${selected}`}</Text>
-        <Text>{`Valor total: R$${(subtotal / 1).toFixed(2)}`}</Text>
+      <Screen name='Parcelas' title='Selecione o número de parcelas'>
+        <ScrollView>
+          <RadioButtonList
+            items={parcelasOptions}
+            onItemSelected={setSelected}
+            initialSelectedItem={selected}
+          />
+          <Text>{`Você selecionou: ${selected}`}</Text>
+          <Text>{`Valor total: R$${(subtotal / 1).toFixed(2)}`}</Text>
+          <Button title="Aplicar" onPress={aplicarParcelas} />
+        </ScrollView>
       </Screen>
     </Navigator>
   )
